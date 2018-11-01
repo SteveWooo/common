96c76fdbd5e7f0ba248123efd95b896c347ce555
461437874@qq.com <461437874@qq.com>
2018-11-01 19:18:21 +0800

    fix config

--- common/init.js
+++ common/init.js
@@ -4,6 +4,13 @@ module.exports = async (type)=>{
 	var swc = {
 		config : config
 	}
+
+	if(swc.config.mode == "local"){
+		swc.config.mq_server.host = "127.0.0.1";
+		swc.config.worker.mq_server_host = "127.0.0.1";
+		swc.config.hsoter.mq_server_host = "127.0.0.1";
+	}
+
 	if(type == "master"){
 		swc = await require("./mq/init_mq")(swc);
 	} else if (type == "hoster"){
