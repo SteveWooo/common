ce8fde00a7bcafdce2b34d1ebd3d5a2665554d40
461437874@qq.com <461437874@qq.com>
2019-03-11 22:45:47 +0800

    fix

--- modules/init_modules.js
+++ modules/init_modules.js
@@ -1,9 +1,5 @@
 module.exports = {
 	demo : require("./demo"),
 	test : require("./test"),
-<<<<<<< HEAD
-	kuaiyaojing : require("./kuaiyaojing"),
-=======
 	downloadSex : require("./downloadSex")
->>>>>>> master
 }
